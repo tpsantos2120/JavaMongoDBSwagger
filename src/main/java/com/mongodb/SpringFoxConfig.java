d1499f7fb2038ed00a7226efaf02d2e40c35e9cf
tpsantos2120 <thiagocroza472@gmail.com>
2021-07-02 16:50:43 -0300

    Fix swagger api path

--- src/main/java/com/mongodb/SpringFoxConfig.java
+++ src/main/java/com/mongodb/SpringFoxConfig.java
@@ -15,7 +15,7 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.mongodb.starter"))
+                .apis(RequestHandlerSelectors.basePackage("com.mongodb"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(info());
