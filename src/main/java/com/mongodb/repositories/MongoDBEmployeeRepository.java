c701ce6599e666f128c96be91db1c2196bf35181
tpsantos2120 <thiagocroza472@gmail.com>
2021-07-02 17:03:28 -0300

    Add implementation of interface to obey contract

---
+++ src/main/java/com/mongodb/repositories/MongoDBEmployeeRepository.java
@@ -0,0 +1,141 @@
+package com.mongodb.repositories;
+
+import com.mongodb.ReadConcern;
+import com.mongodb.ReadPreference;
+import com.mongodb.TransactionOptions;
+import com.mongodb.WriteConcern;
+import com.mongodb.client.ClientSession;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.FindOneAndReplaceOptions;
+import com.mongodb.client.model.ReplaceOneModel;
+import com.mongodb.client.model.WriteModel;
+import com.mongodb.dtos.AverageAge;
+import com.mongodb.models.Employee;
+import org.bson.BsonDocument;
+import org.bson.BsonNull;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.mongodb.client.model.Accumulators.avg;
+import static com.mongodb.client.model.Aggregates.group;
+import static com.mongodb.client.model.Aggregates.project;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.in;
+import static com.mongodb.client.model.Projections.excludeId;
+import static com.mongodb.client.model.ReturnDocument.AFTER;
+import static java.util.Arrays.asList;
+
+@Repository
+public class MongoDBEmployeeRepository implements EmployeeRepository {
+
+    private static final TransactionOptions txnOptions = TransactionOptions.builder()
+            .readPreference(ReadPreference.primary())
+            .readConcern(ReadConcern.MAJORITY)
+            .writeConcern(WriteConcern.MAJORITY)
+            .build();
+    private final MongoClient client;
+    private MongoCollection<Employee> employeeCollection;
+
+    public MongoDBEmployeeRepository(MongoClient mongoClient) {
+        this.client = mongoClient;
+    }
+
+    @PostConstruct
+    void init() {
+        employeeCollection = client.getDatabase("test").getCollection("employees", Employee.class);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        employee.setId(new ObjectId());
+        employeeCollection.insertOne(employee);
+        return employee;
+    }
+
+    @Override
+    public List<Employee> saveAll(List<Employee> employees) {
+        try (ClientSession clientSession = client.startSession()) {
+            return clientSession.withTransaction(() -> {
+                employees.forEach(p -> p.setId(new ObjectId()));
+                employeeCollection.insertMany(clientSession, employees);
+                return employees;
+            }, txnOptions);
+        }
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeCollection.find().into(new ArrayList<>());
+    }
+
+    @Override
+    public List<Employee> findAll(List<String> ids) {
+        return employeeCollection.find(in("_id", mapToObjectIds(ids))).into(new ArrayList<>());
+    }
+
+    @Override
+    public Employee findOne(String id) {
+        return employeeCollection.find(eq("_id", new ObjectId(id))).first();
+    }
+
+    @Override
+    public long count() {
+        return employeeCollection.countDocuments();
+    }
+
+    @Override
+    public long delete(String id) {
+        return employeeCollection.deleteOne(eq("_id", new ObjectId(id))).getDeletedCount();
+    }
+
+    @Override
+    public long delete(List<String> ids) {
+        try (ClientSession clientSession = client.startSession()) {
+            return clientSession.withTransaction(
+                    () -> employeeCollection.deleteMany(clientSession, in("_id", mapToObjectIds(ids))).getDeletedCount(),
+                    txnOptions);
+        }
+    }
+
+    @Override
+    public long deleteAll() {
+        try (ClientSession clientSession = client.startSession()) {
+            return clientSession.withTransaction(
+                    () -> employeeCollection.deleteMany(clientSession, new BsonDocument()).getDeletedCount(), txnOptions);
+        }
+    }
+
+    @Override
+    public Employee update(Employee employee) {
+        FindOneAndReplaceOptions options = new FindOneAndReplaceOptions().returnDocument(AFTER);
+        return employeeCollection.findOneAndReplace(eq("_id", employee.getId()), employee, options);
+    }
+
+    @Override
+    public long update(List<Employee> employees) {
+        List<WriteModel<Employee>> writes = employees.stream()
+                .map(p -> new ReplaceOneModel<>(eq("_id", p.getId()), p))
+                .collect(Collectors.toList());
+        try (ClientSession clientSession = client.startSession()) {
+            return clientSession.withTransaction(
+                    () -> employeeCollection.bulkWrite(clientSession, writes).getModifiedCount(), txnOptions);
+        }
+    }
+
+    @Override
+    public double getAverageAge() {
+        List<Bson> pipeline = asList(group(new BsonNull(), avg("averageAge", "$age")), project(excludeId()));
+        return employeeCollection.aggregate(pipeline, AverageAge.class).first().getAverageAge();
+    }
+
+    private List<ObjectId> mapToObjectIds(List<String> ids) {
+        return ids.stream().map(ObjectId::new).collect(Collectors.toList());
+    }
+}
